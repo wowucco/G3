21db94dd8ff3515a6bfc48329f11f0de5950cf09
Володимир Щербак <vshcherbak@ip-192-168-88-170.eu-central-1.compute.internal>
2020-10-21 13:17:01 +0300

    docker base

---
+++ cmd/api/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+exec > >(tee -a /var/log/app/entry.log|logger -t server -s 2>/dev/console) 2>&1
+
+echo "[`date`] Running entrypoint script..."
+
+echo "[`date`] Starting server..."
+./api
